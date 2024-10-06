1fe78228632f80fbc0ec24a373f64e851122471f
Rajdeep <rajdeepmukherjee2002@gmail.com>
2024-10-06 22:34:18 +0530

    added more end points and service

--- src/main/java/com/rajsubhod/userservice/controller/UserController.java
+++ src/main/java/com/rajsubhod/userservice/controller/UserController.java
@@ -1,31 +1,134 @@
 package com.rajsubhod.userservice.controller;
 
 import com.rajsubhod.userservice.dto.UserInfoDto;
+import com.rajsubhod.userservice.entity.UserInfo;
 import com.rajsubhod.userservice.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user/api")
 @RequiredArgsConstructor
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
+
+    /**
+     * <p>
+     * This method is used to get all the users
+     * </p>
+     * Accessible by only admin
+     * @return List of users
+     */
     @GetMapping("/v1/")
     public ResponseEntity<?> getUser(){
+        logger.info("Fetching all users");
         return new ResponseEntity<>(userService.getUserInfo(), HttpStatus.OK);
     }
 
-    @GetMapping("/v1/{userId}")
-    public ResponseEntity<?> getUserById(@PathVariable String userId){
+    /**
+     * <p>
+     * This method is used to save the user
+     * </p>
+     * Accessible by only admin
+     * </p>
+     * @param userInfo
+     * @return
+     */
+
+    @PostMapping("/v1/")
+    public ResponseEntity<?> saveUser(@RequestBody UserInfo userInfo){
+        logger.info("Saving user: {}", userInfo);
+        userService.saveUserInfo(userInfo);
+        return new ResponseEntity<>("User saved successfully", HttpStatus.CREATED);
+    }
+
+    /**
+     * <p>
+     * This method is used to save a list of users
+     * </p>
+     * Accessible by only admin
+     * </p>
+     * @param userInfos
+     * @return
+     */
+    @PostMapping("/v1/list")
+    public ResponseEntity<?> saveUsers(@RequestBody List<UserInfo> userInfos) {
+        logger.info("Saving list of users: {}", userInfos.toString());
+        userInfos.forEach(userService::saveUserInfo);
+        return new ResponseEntity<>("Users saved successfully", HttpStatus.CREATED);
+    }
+
+    /**
+     * <p>
+     * This method is used to delete the user
+     * </p>
+     * Accessible by only admin
+     * </p>
+     * @param userId
+     * @return
+     */
+
+    @DeleteMapping("/v1/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable String userId){
+        logger.info("Deleting user with ID: {}", userId);
+        userService.deleteUserInfo(userId);
+        return new ResponseEntity<>("User deleted successfully", HttpStatus.OK);
+    }
+
+    /**
+     * <p>
+     * This method is used to get the user by userId
+     * </p>
+     * Accessible by admin for any users and user for self
+     * </p>
+     * @param userId
+     * @param request
+     * @return UserInfoDto
+     */
+
+    @GetMapping({"/v1/","/v1/{userId}"})
+    public ResponseEntity<?> getUserById(@PathVariable(required = false) String userId, HttpServletRequest request){
+        userId = request.getHeader("userId");
+        if(userId == null || userId.isEmpty()){
+            logger.warn("User ID is required but not provided");
+            return new ResponseEntity<>("User Id is required", HttpStatus.BAD_REQUEST);
+        }
+        logger.info("Fetching user with ID: {}", userId);
         return new ResponseEntity<>(userService.getUserInfo(userId), HttpStatus.OK);
     }
 
-    @PutMapping("/v1/{userId}")
-    public ResponseEntity<?> updateUser(@PathVariable String userId, @RequestBody UserInfoDto userInfoDto){
+    /**
+     * <p>
+     * This method is used to update the user
+     * </p>
+     * Accessible by only admin
+     * </p>
+     * @param userId
+     * @param userInfoDto
+     * @param requestUserId
+     * @return UserInfoDto
+     */
+
+    @PutMapping({"/vi/","/v1/{userId}"})
+    public ResponseEntity<?> updateUser(@PathVariable(required = false) String userId, @RequestBody UserInfoDto userInfoDto, @RequestHeader("userId") String requestUserId){
+        if(userId == null || userId.isEmpty()){
+            userId = requestUserId;
+        }
+        if(userId == null || userId.isEmpty()){
+            logger.warn("User ID is required but not provided");
+            return new ResponseEntity<>("User Id is required", HttpStatus.BAD_REQUEST);
+        }
+        logger.info("Updating user with ID: {}", userId);
         userService.updateUserInfo(userId, userInfoDto);
         return new ResponseEntity<>("User updated successfully", HttpStatus.OK);
     }
--- src/main/java/com/rajsubhod/userservice/service/UserService.java
+++ src/main/java/com/rajsubhod/userservice/service/UserService.java
@@ -3,7 +3,10 @@ package com.rajsubhod.userservice.service;
 import com.rajsubhod.userservice.dto.UserInfoDto;
 import com.rajsubhod.userservice.entity.UserInfo;
 import com.rajsubhod.userservice.repository.UserInfoRepository;
+import com.rajsubhod.userservice.utils.UserInfoToDtoConverter;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,44 +17,68 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
 
-
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
     private final UserInfoRepository userInfoRepository;
+    private final UserInfoToDtoConverter userInfoToDtoConverter;
 
 
+    /**
+     * Save user info
+     * @param userInfo
+     */
     public void saveUserInfo(UserInfo userInfo) {
+        logger.info("Saving user info");
         userInfoRepository.save(userInfo);
     }
 
+    /**
+     * Update user info
+     * @param userId
+     * @param userInfoDto
+     */
     public void updateUserInfo(String userId, UserInfoDto userInfoDto) {
+        logger.info("Updating user info");
         Optional<UserInfo> userDetails = userInfoRepository.findByUserId(userId);
         if(userDetails.isPresent()){
             UserInfo updatedUserInfo = userDetails.get();
             updatedUserInfo.setFirstName(userInfoDto.getFirstName());
             updatedUserInfo.setLastName(userInfoDto.getLastName());
-            updatedUserInfo.setPhoneNumber(userInfoDto.getPhoneNumber());
+            updatedUserInfo.setPhoneNumber(Long.valueOf(userInfoDto.getPhoneNumber()));
             userInfoRepository.save(updatedUserInfo);
         } else {
+            logger.error("User not found");
             throw new RuntimeException("User not found");
         }
     }
 
-    private UserInfoDto convertToDto(UserInfo userInfo) {
-        return new UserInfoDto(
-                userInfo.getUsername(),
-                userInfo.getFirstName(),
-                userInfo.getLastName(),
-                userInfo.getEmail(),
-                userInfo.getPhoneNumber()
-        );
-    }
-
+    /**
+     * Get user info
+     * @param userId
+     * @return user info
+     */
     public UserInfoDto getUserInfo(String userId) {
-
-        return userInfoRepository.findByUserId(userId).map(this::convertToDto).orElseThrow(()-> new RuntimeException("User not found"));
+        logger.info("Getting user info");
+        return userInfoRepository.findByUserId(userId).map(userInfoToDtoConverter::convertToDto).orElseThrow(()->{
+            logger.error("User not found");
+            return new RuntimeException("User not found");
+        });
     }
 
+    /**
+     * Get all user info
+     * @return list of user info
+     */
     public List<UserInfo> getUserInfo() {
+        logger.info("Getting all user info");
         return userInfoRepository.findAll();
     }
 
+    /**
+     * Delete user info
+     * @param userId
+     */
+    public void deleteUserInfo(String userId) {
+        logger.info("Deleting user info");
+        userInfoRepository.deleteById(userId);
+    }
 }
